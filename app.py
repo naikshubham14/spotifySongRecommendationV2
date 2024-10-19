a319bb417950541bafc6bdb3172119e5832b3a35
Shubham Naik <shubhamnaik321@gmail.com>
2024-10-20 03:51:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def n_neighbors_uri_audio(genre, start_year, end_year, test_feat):
     return uris, audios
 
 
-title = "Song Recommendation Engine"
+title = "PlaylistPilot: Your Song Recommendation Engine"
 st.title(title)
 
 st.write("First of all, welcome! This is the place where you can customize what you want to listen to based on genre and several key audio features. Try playing around with different settings and listen to the songs recommended by our system!")
